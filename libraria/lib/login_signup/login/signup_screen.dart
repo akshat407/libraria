b2cee7c2f0ffbb90059f1c7233fb41e74ef21f82
akshat407 <srivastava.akshat407@gmail.com>
2024-01-15 22:46:03 +0530

    Google signin needs to be fixed

--- libraria/lib/login_signup/login/signup_screen.dart
+++ libraria/lib/login_signup/login/signup_screen.dart
@@ -1,3 +1,7 @@
+import 'package:custom_signin_buttons/button_data.dart';
+import 'package:custom_signin_buttons/button_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:libraria/navbar/home/aboutus.dart';
@@ -6,14 +10,35 @@ import 'package:libraria/login_signup/login/Login.dart';
 import 'package:libraria/login_signup/login/register1.dart';
 
 import 'package:libraria/navbar/news/news_main.dart';
+import 'package:libraria/utils/color.dart';
 // import 'package:libraria/navbar/news/news_main.dart';
 
 
 
 
 
-class loginsignup extends StatelessWidget {
+class loginsignup extends StatefulWidget {
   const loginsignup({super.key});
+
+  @override
+  State<loginsignup> createState() => _loginsignupState();
+}
+
+class _loginsignupState extends State<loginsignup> {
+
+final FirebaseAuth auth=FirebaseAuth.instance;
+User? user;
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    auth.authStateChanges().listen((event) {
+      setState(() {
+        user=event;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -102,64 +127,115 @@ class loginsignup extends StatelessWidget {
                   borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
                   color: Colors.white,
                   border: Border.all(color: Colors.black)),
+              // child: Center(
+              //   child: Column(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       Row(
+              //         mainAxisAlignment: MainAxisAlignment.center,
+              //         children: [
+              //           Container(
+              //             decoration: BoxDecoration(
+              //               image: DecorationImage(image: AssetImage('assets/images/facebook_logo.png'),fit: BoxFit.cover)
+              //             ),
+
+              //           ),
+              //           TextButton(
+              //             onPressed: (){
+              //               Get.to(() => home());
+              //             }, 
+              //             child: Text("Sign in with Facebook",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)),
+                          
+              //           // Text(
+              //           //   "official digital library of BMSCE",
+              //           //   style: TextStyle(
+              //           //       fontSize: 20,
+              //           //       fontWeight: FontWeight.w700,
+              //           //       color: Colors.white),
+              //           // ),
+              //         ],
+              //       ),
+              //       Row(
+              //         mainAxisAlignment: MainAxisAlignment.center,
+              //         children: [
+
+              //           Container(
+              //             decoration: BoxDecoration(
+              //               image: DecorationImage(image: AssetImage('assets/images/facebook_logo.png'))
+              //             ),
+
+              //           ),
+              //           TextButton(
+              //             onPressed: (){
+              //             // Get.to(newsmain());
+              //             }, 
+              //             child: Text("Sign in with Google",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)),
+                          
+              //           // Text(
+              //           //   "official digital library of BMSCE",
+              //           //   style: TextStyle(
+              //           //       fontSize: 20,
+              //           //       fontWeight: FontWeight.w700,
+              //           //       color: Colors.white),
+              //           // ),
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              // )
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(image: AssetImage('assets/images/facebook_logo.png'),fit: BoxFit.cover)
-                          ),
-
-                        ),
-                        TextButton(
-                          onPressed: (){
-                            Get.to(() => home());
-                          }, 
-                          child: Text("Sign in with Facebook",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)),
-                          
-                        // Text(
-                        //   "official digital library of BMSCE",
-                        //   style: TextStyle(
-                        //       fontSize: 20,
-                        //       fontWeight: FontWeight.w700,
-                        //       color: Colors.white),
-                        // ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-
-                        Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(image: AssetImage('assets/images/facebook_logo.png'))
-                          ),
-
-                        ),
-                        TextButton(
-                          onPressed: (){
-                          // Get.to(newsmain());
-                          }, 
-                          child: Text("Sign in with Google",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)),
-                          
-                        // Text(
-                        //   "official digital library of BMSCE",
-                        //   style: TextStyle(
-                        //       fontSize: 20,
-                        //       fontWeight: FontWeight.w700,
-                        //       color: Colors.white),
-                        // ),
-                      ],
-                    ),
-                  ],
-                ),
-              )),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              SignInButton(
+                button: Button.Facebook,
+                width: 300,
+                borderRadius: 30,
+                textSize: 18,
+                iconSize: 30,
+                // spanish: true,
+                text: "Continue with facebook",
+                onPressed: () {},
+                // splashColor: Colors.red,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SignInButton(
+                button: Button.Google,
+                width: 300,
+                borderRadius: 30,
+                textSize: 20,
+                iconSize: 30,
+                text: "Continue with Google",
+                onPressed: () {
+                  handleGoogleSign();
+                },
+                splashColor: AIColors.primaryColor2,
+                showText: true,
+              ),
+            ]
+          )
+              )
+              ),
         ],
       ),
     ));
+  }
+
+  void handleGoogleSign()
+  {
+    try{
+      GoogleAuthProvider googleAuthProvider=GoogleAuthProvider();
+      auth.signInWithProvider(googleAuthProvider);
+
+
+    }
+    catch(error)
+    {
+      print(error);
+    }
   }
 }
