066245c822201dfcfcfe66ee66d4321248928f71
akshat407 <srivastava.akshat407@gmail.com>
2024-01-26 01:56:04 +0530

    Chnage password

---
+++ libraria/lib/navbar/settings/change_pswd.dart
@@ -0,0 +1,80 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class chngepswd extends StatefulWidget {
+  const chngepswd({super.key});
+
+  @override
+  State<chngepswd> createState() => _chngepswdState();
+}
+
+class _chngepswdState extends State<chngepswd> {
+  get oldPassword => null;
+
+  @override
+  Widget build(BuildContext context) {
+
+    var auth=FirebaseAuth.instance;
+    var currentUser=FirebaseAuth.instance.currentUser;
+
+    changePassword({email,oldPassword,newPassword}) async{
+
+      var cred=EmailAuthProvider.credential(email: email, password: oldPassword);
+
+      await currentUser!.reauthenticateWithCredential(cred).then((value){
+        currentUser.updatePassword(newPassword);
+      }).catchError((error){
+
+        print(error.toString());
+
+      });
+
+    }
+
+    var oldPasswordController=TextEditingController();
+    var newPasswordController=TextEditingController();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: oldPasswordController,
+              decoration: InputDecoration(
+                isDense: true,
+                alignLabelWithHint: true,
+                labelText: "Old Password",
+                hintText: "Enter your old password",
+              ),
+            ),
+              TextFormField(
+              controller: oldPasswordController,
+              decoration: InputDecoration(
+                isDense: true,
+                alignLabelWithHint: true,
+                labelText: "Old Password",
+                hintText: "Enter your old password",
+              ),
+              ),
+              ElevatedButton(
+                onPressed: ()async{
+                  await changePassword(
+                  email:  ,
+                  oldPassword: oldPasswordController.text,
+                  newPassword: newPasswordController.text,
+
+                  );
+                  print('Password changed');
+                  
+
+                }, 
+                
+                child: Text("Change Password"))
+          ],
+        ),
+      ),
+
+    );
+  }
+}
--- libraria/lib/navbar/settings/settings_main.dart
+++ libraria/lib/navbar/settings/settings_main.dart
@@ -1,7 +1,9 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:libraria/navbar/settings/change_pswd.dart';
 import 'package:libraria/utils/color.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -113,7 +115,7 @@ class _setting_mainState extends State<setting_main> {
               children: [
                 InkWell(
                   onTap: () {
-                    
+                    Get.to(chngepswd());
                   },
                   child: Container(
                     padding: EdgeInsets.only(top: 20),
